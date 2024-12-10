0dbaafd992d8e769f41d701eb711909bf7221323
ermadhav <madhavtiwari2024@gmail.com>
2024-12-11 04:20:39 +0530

    almost done

--- app.js
+++ app.js
@@ -3,23 +3,39 @@ const app = express();
 const userModel = require("./models/user");
 const postModel = require("./models/post");
 const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt'); 
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
+const crypto = require("crypto");
+const path = require("path");
+const multer = require("multer");
+const upload = require('./config/multerconfig');
 
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.render("index");
 });
 
-app.post('/register', async (req,res) => {
-    let{email, password, username, age, name} = req.body;
-    let user = await userModel.findOne({email});
-    if(user) return res.status(500).send("User Already Exists");
+app.get('/profile/upload',(req, res) => {
+    res.render("profileupload");
+});
+
+app.post('/upload', isLoggedIn ,upload.single("profilepic") , async (req, res) => {
+    let user = await userModel.findOne({email: req.user.email});
+    user.profilepic = req.file.filename;
+    await user.save();
+    res.redirect("/profile")
+
+});
+
+app.post('/register', async (req, res) => {
+    let { email, password, username, age, name } = req.body;
+    let user = await userModel.findOne({ email });
+    if (user) return res.status(500).send("User Already Exists");
 
     bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(password, salt, async (err, hash) => {
@@ -31,30 +47,30 @@ app.post('/register', async (req,res) => {
                 password: hash
             });
 
-            let token = jwt.sign({ email: email, userid:user._id}, "shhhh");
+            let token = jwt.sign({ email: email, userid: user._id }, "shhhh");
             res.cookie("token", token);
             res.send("Registered")
         })
     })
 });
 
-app.get('/login',  (req, res) => {
+app.get('/login', (req, res) => {
     res.render("login");
 });
 
-app.get("/profile", isLoggedIn, async (req,res) => {
-    let user = await userModel.findOne({email: req.user.email}).populate("posts")
-   
-    res.render("profile", {user});
+app.get("/profile", isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({ email: req.user.email }).populate("posts")
+
+    res.render("profile", { user });
 });
 
-app.get("/like/:id", isLoggedIn, async (req,res) => {
-    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user");
 
-    if(post.likes.indexOf(req.user.userid) === -1){
+    if (post.likes.indexOf(req.user.userid) === -1) {
         post.likes.push(req.user.userid);
     }
-    else{
+    else {
         post.likes.splice(post.likes.indexOf(req.user.userid), 1);
     }
 
@@ -63,21 +79,21 @@ app.get("/like/:id", isLoggedIn, async (req,res) => {
     res.redirect("/profile");
 });
 
-app.get("/edit/:id", isLoggedIn, async (req,res) => {
-    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+app.get("/edit/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user");
 
-    res.render("edit", {post});
+    res.render("edit", { post });
 });
 
-app.post("/update/:id", isLoggedIn, async (req,res) => {
-    let post = await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content});
+app.post("/update/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOneAndUpdate({ _id: req.params.id }, { content: req.body.content });
 
     res.redirect("/profile");
 });
 
-app.post("/post", isLoggedIn, async (req,res) => {
-    let user = await userModel.findOne({email: req.user.email});
-    let {content} = req.body;
+app.post("/post", isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({ email: req.user.email });
+    let { content } = req.body;
 
 
     let post = await postModel.create({
@@ -89,14 +105,14 @@ app.post("/post", isLoggedIn, async (req,res) => {
     res.redirect("/profile")
 });
 
-app.post('/login', async (req,res) => {
-    let{email, password} = req.body;
-    let user = await userModel.findOne({email});
-    if(!user) return res.status(500).send("Something Went Wrong");
+app.post('/login', async (req, res) => {
+    let { email, password } = req.body;
+    let user = await userModel.findOne({ email });
+    if (!user) return res.status(500).send("Something Went Wrong");
 
     bcrypt.compare(password, user.password, (err, result) => {
-        if(result) {
-            let token = jwt.sign({ email: email, userid:user._id}, "shhhh");
+        if (result) {
+            let token = jwt.sign({ email: email, userid: user._id }, "shhhh");
             res.cookie("token", token);
             res.status(200).redirect("/profile");
 
@@ -105,13 +121,13 @@ app.post('/login', async (req,res) => {
     })
 });
 
-app.get('/logout', (req,res)=> {
+app.get('/logout', (req, res) => {
     res.cookie("token", "");
     res.redirect("login");
 })
 
-function isLoggedIn(req, res, next){
-    if(req.cookies.token === "") res.redirect("/login");
+function isLoggedIn(req, res, next) {
+    if (req.cookies.token === "") res.redirect("/login");
     else {
         const data = jwt.verify(req.cookies.token, "shhhh");
         req.user = data;
