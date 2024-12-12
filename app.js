9d7887430e247920c25bc0c53925ed679f7f9268
ermadhav <madhavtiwari2024@gmail.com>
2024-12-12 19:59:37 +0530

    Changed the UI of the app

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+
 app.get('/', (req, res) => {
     res.render("index");
 });
@@ -29,7 +30,6 @@ app.post('/upload', isLoggedIn ,upload.single("profilepic") , async (req, res) =
     user.profilepic = req.file.filename;
     await user.save();
     res.redirect("/profile")
-
 });
 
 app.post('/register', async (req, res) => {
@@ -60,7 +60,6 @@ app.get('/login', (req, res) => {
 
 app.get("/profile", isLoggedIn, async (req, res) => {
     let user = await userModel.findOne({ email: req.user.email }).populate("posts")
-
     res.render("profile", { user });
 });
 
@@ -75,26 +74,22 @@ app.get("/like/:id", isLoggedIn, async (req, res) => {
     }
 
     await post.save();
-    // post.likes.push(req.user.userid);
     res.redirect("/profile");
 });
 
 app.get("/edit/:id", isLoggedIn, async (req, res) => {
     let post = await postModel.findOne({ _id: req.params.id }).populate("user");
-
     res.render("edit", { post });
 });
 
 app.post("/update/:id", isLoggedIn, async (req, res) => {
     let post = await postModel.findOneAndUpdate({ _id: req.params.id }, { content: req.body.content });
-
     res.redirect("/profile");
 });
 
 app.post("/post", isLoggedIn, async (req, res) => {
     let user = await userModel.findOne({ email: req.user.email });
     let { content } = req.body;
-
 
     let post = await postModel.create({
         user: user._id,
@@ -115,7 +110,6 @@ app.post('/login', async (req, res) => {
             let token = jwt.sign({ email: email, userid: user._id }, "shhhh");
             res.cookie("token", token);
             res.status(200).redirect("/profile");
-
         }
         else res.redirect("/login");
     })
@@ -124,7 +118,17 @@ app.post('/login', async (req, res) => {
 app.get('/logout', (req, res) => {
     res.cookie("token", "");
     res.redirect("login");
-})
+});
+app.post("/updateProfile", isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({ email: req.user.email });
+    let { name, email } = req.body;
+
+    user.name = name;
+    user.email = email;
+    await user.save();
+    res.redirect("/profile");
+});
+
 
 function isLoggedIn(req, res, next) {
     if (req.cookies.token === "") res.redirect("/login");
@@ -135,4 +139,6 @@ function isLoggedIn(req, res, next) {
     }
 }
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server is listening at 3000");
+});
